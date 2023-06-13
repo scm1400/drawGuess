29e15761f7ea2de4574ee0615052bbe4890ef3c4
D.심은철 <scm1400@gmail.com>
2023-06-14 00:58:02 +0900

    캐치마인드 기본UI, 지우개기능, 패킷 batch 전송 개발

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,22 @@ import { ScriptPlayer } from "zep-script";
 
 ScriptApp.onJoinPlayer.Add(function (player: ScriptPlayer) {
 	player.tag = {};
-	player.tag.widget = player.showWidget("canvas.html", "middle", 600, 600);
+	player.tag.widget = player.showWidget("canvas.html", "middle", 800, 450);
 	//@ts-ignore
 	player.tag.widget.onMessage.Add(function (player, data) {
 		if (data.type == "sendDrawingData") {
 			for (const p of ScriptApp.players) {
+				if(p === player) continue;
 				if (p.tag.widget) {
 					data.type = "drawingNotify";
+					p.tag.widget.sendMessage(data);
+				}
+			}
+		}else if (data.type == "batchedDrawingData") {
+			for (const p of ScriptApp.players) {
+				if(p === player) continue;
+				if (p.tag.widget) {
+					data.type = "batchedDrawingData";
 					p.tag.widget.sendMessage(data);
 				}
 			}
